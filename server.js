489862ddaa809612d89a628ba6d3a40cadc0f9fa
sebastianbuscaglia <sebastian.buscaglia@magneticash.com>
2024-06-05 16:57:57 -0300

    cm

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ expressApp.get('/devices', async (req, res) => {
     const response = await axios.get('https://api.mercadopago.com/point/integration-api/devices', {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${mp.access_token}`
+        'Authorization': `Bearer APP_USR-1069336669952081-052209-9fa64ea807043c53cedad2a04a9c600b-219117670`
       }
     });
 
@@ -73,8 +73,46 @@ expressApp.get('/devices', async (req, res) => {
   }
 });
 
+// Ruta para realizar un pago con un dispositivo Point
+expressApp.post('/devices/:deviceid/payment-intents', async (req, res) => {
+  const { deviceid } = req.params;
+  const { amount, external_reference, print_on_terminal, ticket_number } = req.body;
+
+  try {
+    const response = await axios.post(`https://api.mercadopago.com/point/integration-api/devices/${deviceid}/payment-intents`, {
+      amount,
+      additional_info: {
+        external_reference,
+        print_on_terminal,
+        ticket_number
+      }
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer APP_USR-1069336669952081-052209-9fa64ea807043c53cedad2a04a9c600b-219117670`
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error creating payment intent:', error);
+    res.status(500).send(error.message);
+  }
+});
+
 // Iniciar el servidor Express
 const PORT = process.env.PORT || 3000;
 expressApp.listen(PORT, () => {
   console.log(`Servidor Express escuchando en el puerto ${PORT}`);
 });
+
+
+// "devices": [
+//   {
+//       "id": "PAX_A910__SMARTPOS1491307350",
+//       "pos_id": 101159411,
+//       "store_id": "61421177",
+//       "external_pos_id": "",
+//       "operating_mode": "STANDALONE"
+//   }
+// ],
